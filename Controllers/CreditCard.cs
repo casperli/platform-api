036b9ca8cded008dcd3d0e85b54f0bfbd18ae93b
Andreas Caspers <casperli@noreply.users.github.com>
2018-08-17 07:52:19 +0200

    First code

---
+++ Controllers/CreditCard.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImmoPortalApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CreditCard : Controller
+    {
+        [HttpGet()]
+        public IActionResult Get(CreditCardCheckRequest request)
+        {
+            var result = new CreditCardCheckResponse
+            {
+                CardNumber = request.CardNumber,
+                IsValid = true
+            };
+            return new JsonResult(result);
+        }
+    }
+
+    public class CreditCardCheckRequest
+    {
+        public string Name { get; set; }
+
+        [CreditCard] public string CardNumber { get; set; }
+
+        public string Csv { get; set; }
+
+        public string ValidThrough { get; set; }
+    }
+
+    public class CreditCardCheckResponse
+    {
+        public bool IsValid { get; set; }
+
+        public string CardNumber { get; set; }
+    }
+}
